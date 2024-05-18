bb5244a1e54bc4e3c9fc4efe9809cb194cba302d
tufusa <me@tufusa.net>
2024-05-18 10:31:59 +0900

    feat: OGP対応 (#28)
    * feat: レイアウトにtitle, description, imageを渡せるように変更
    
    * feat: 各ページにtitleとdescriptionを付与
    
    * chore: imageにデフォルトでfaviconを渡す
    
    * fix: typo
    
    * feat: OGPタグを設定
    
    * fix: OGP画像のパスを修正
    
    * fix: OGP画像のパスを修正
    
    * fix: OGP画像のパスを修正
    Astroに解決させられそうにないのでHTMLテンプレートに直書きする
    
    * fix: OPG画像のパスを絶対パスに修正
    
    * fix: OGP画像をpngに変更
    svgは使えない

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon({ iconDir: 'src/assets' })],
+  site: 'https://tufusa.net',
 });
