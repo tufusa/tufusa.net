6a2ad8bffa681b6a08ce76df7ea5f69b096aa5d8
tufusa <me@tufusa.net>
2024-06-01 20:45:16 +0900

    feat: note機能を作成 (#36)
    * chore: prettier
    
    * chore: フォントのインポートをCSSファイルに移動
    Route159を追加
    
    * chore: デフォルトの英数字にRoute159を使用
    
    * chore: Route159を追加
    
    * chore: 不要なフォント指定を削除
    
    * chore: アンダーラインをフォントから設定
    
    * chore: ノートのスタイリング
    
    * feat: ノート一覧ページ
    
    * chore: ノートを追加
    
    * fix(ui): 文字サイズを小さくしない
    
    * chore: ノートを更新
    
    * chore: robots.txtを/note以下を見ないように更新

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,22 +8,17 @@ export default {
         back: '#121222',
       },
       height: {
-        screen: [
-          '100vh', '100dvh'
-        ]
+        screen: ['100vh', '100dvh'],
       },
       minHeight: {
-        screen: [
-          '100vh', '100dvh'
-        ]
+        screen: ['100vh', '100dvh'],
       },
       maxHeight: {
-        screen: [
-          '100vh', '100dvh'
-        ]
+        screen: ['100vh', '100dvh'],
       },
       fontFamily: {
         sans: [
+          'Route159',
           '"Noto Sans JP"',
           'ui-sans-serif',
           'system-ui',
