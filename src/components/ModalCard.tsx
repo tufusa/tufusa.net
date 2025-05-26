886ebec33b9d99f73db0693cf575fc9d4ca72341
tufusa <tufusa74@gmail.com>
2025-05-26 22:03:30 +0900

    chore: モーダル背景を拡大

--- src/components/ModalCard.tsx
+++ src/components/ModalCard.tsx
@@ -8,7 +8,7 @@ export const ModalCard = ({ button, modal }: { button?: ReactElement; modal?: Re
       <button onClick={() => setIsOpen(true)}>{button}</button>
       {isOpen && (
         <div className="fixed inset-0 z-30 flex items-center justify-center">
-          <div className="fixed inset-0 bg-gray-800/50" onClick={() => setIsOpen(false)} />
+          <div className="fixed -inset-16 bg-gray-800/50" onClick={() => setIsOpen(false)} />
           <div className="z-40">{modal}</div>
         </div>
       )}
