bba5dc1237d668ba3bca99f81b2431d91e0e9025
tufusa <tufusa74@gmail.com>
2025-05-26 13:51:03 +0900

    feat: `/nazo`ページを追加

---
+++ src/components/ModalCard.tsx
@@ -0,0 +1,17 @@
+import { useState, type ReactElement } from 'react';
+
+export const ModalCard = ({ button, modal }: { button?: ReactElement; modal?: ReactElement }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>{button}</button>
+      {isOpen && (
+        <div className="fixed inset-0 z-30 flex items-center justify-center">
+          <div className="fixed inset-0 bg-gray-800/50" onClick={() => setIsOpen(false)} />
+          <div className="z-40">{modal}</div>
+        </div>
+      )}
+    </>
+  );
+};
